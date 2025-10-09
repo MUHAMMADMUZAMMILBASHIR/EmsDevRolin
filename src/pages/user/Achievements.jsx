e168bca8a80984333d77029547db92168195032d
muzammil09dev <muzammildev0099@gmail.com>
2025-10-09 12:28:25 +0500

    ems frontend

---
+++ src/pages/user/Achievements.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+import Layout from '../../components/Layout'
+import Achievements from '../../components/user/Achievements'
+
+export default function Achievement() {
+  return (
+    <Layout>
+      <Achievements/>
+    </Layout>
+  )
+}
